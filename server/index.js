e665a007511329b1e3986661433d5d5ee61cd3b0
shirleyZ <tribble1998@gmail.com>
2024-12-13 20:16:21 +0000

    try

--- server/index.js
+++ server/index.js
@@ -5,6 +5,9 @@ import cors from "cors";
 import dotenv from "dotenv";
 import helmet from "helmet";
 import morgan from "morgan";
+import kpiRoutes from "./routers/kpi.js";
+import KPI from "./model/KPI.js";
+import{ kpis } from "./data/data.js";
 
 dotenv.config();
 const app = express();
@@ -15,8 +18,10 @@ app.use(morgan("common"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
+app.use("/kpis", kpiRoutes);
 
 const PORT = process.env.PORT;
+
 
 mongoose
     .connect(process.env.MONGO_URL, {
@@ -25,33 +30,12 @@ mongoose
     })
     .then(async () => {
         app.listen(PORT, () => console.log(`server port: ${PORT}`));
+
+
+        // ADD DATA ONE TIME ONLY OR AS NEEDED
+        await mongoose.connection.db.dropDatabase();
+        await KPI.insertMany(kpis);
+
+
     })
     .catch((error) => console.log(`${error} did not connect`));
-
-// mongoose.set('debug', true);  // 启用 Mongoose 调试模式
-
-
-// async function connectToMongoDB() {
-//   try {
-//     await mongoose.connect(process.env.MONGO_URL, {
-//       useNewUrlParser: true,
-//       useUnifiedTopology: true,
-//       serverSelectionTimeoutMS: 10000, // 增加超时时间
-//     });
-//     console.log('成功连接到 MongoDB');
-//   } catch (error) {
-//     console.error('MongoDB连接错误:', error);
-    
-//     // 详细错误诊断
-//     if (error.code === 'ESERVFAIL') {
-//       console.error('DNS解析失败，可能的原因：');
-//       console.error('1. 网络连接问题');
-//       console.error('2. DNS服务器配置错误');
-//       console.error('3. MongoDB Atlas 集群配置问题');
-//     }
-    
-//     process.exit(1);
-//   }
-// }
-
-// connectToMongoDB();
